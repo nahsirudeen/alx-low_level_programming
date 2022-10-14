2fb86188c0b3d442323d2889c4a1819d750bed83
nahsirudeen <arikewuyonasiru@gmail.com>
2022-10-13 17:07:23 -0700

    task_4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ * Return: By default returns zero if no error (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
+	return (0);
+}
