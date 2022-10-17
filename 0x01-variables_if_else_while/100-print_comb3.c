cda71dada72fdc00d4f10da41e977b7ccf8c3bac
nahsirudeen <arikewuyonasiru@gmail.com>
2022-10-17 12:19:54 -0700

    task_10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i, k = '0', j;
+
+	for (i= '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
