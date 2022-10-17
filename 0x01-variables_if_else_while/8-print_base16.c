a0d9c6c4a9d1362fc62514e0cddc03c69775c898
nahsirudeen <arikewuyonasiru@gmail.com>
2022-10-16 18:25:30 -0700

    task_8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return:0 if pogram is correct return non zero if false
+ */
+int main(void)
+{
+	unsigned char a = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
