6654d629dc589d482891f3129c6bb6ecc3c92468
nahsirudeen <arikewuyonasiru@gmail.com>
2022-10-19 19:41:58 -0700

    task_12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,6 +12,9 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
 			op = a * b;
 			_putchar(44);
 			_putchar(32);
@@ -36,4 +39,5 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
+}
 }
