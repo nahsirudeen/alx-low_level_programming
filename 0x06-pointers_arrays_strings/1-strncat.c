ccfe3a3d0afb1b98af7ca19d6d7bf0d1dbedde19
nahsirudeen <arikewuyonasiru@gmail.com>
2022-10-27 18:10:40 -0700

    tsk_1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: destinstion
+ * @src: source
+ * @n: amount of the bytes used from src
+ * Return: the pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
