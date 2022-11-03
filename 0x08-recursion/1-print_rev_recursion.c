b7941d26ce7a9d8c3aee7ea867070dba959c0a2e
arikewuyo nasirudeen <113855956+nahsirudeen@users.noreply.github.com>
2022-11-03 16:04:43 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return.
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
  */
-void _puts_recursion(char *s)
+
+void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
 }
