d2efe5e704437e526dee335b05f304ea906c039b
nahsirudeen <arikewuyonasiru@gmail.com>
2022-11-10 17:51:43 -0800

    task_0

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,13 +18,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		c2++;
 	if (c1 >= c2)
 		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
+<F12>	else
+<F12>		bg = c2;
+<F12>	if (size_r <= bg + 1)
+<F12>		return (0);
+<F12>	r[bg + 1] = '\0';
 	c1 --, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48 dr2 = *(n2 + c2) - 48;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
@@ -35,9 +35,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (op > 0)
 			*(r + bg) = (op % 10) + 48;
 		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
+<F12>			*(r + bg) = '0';
+<F12>		if (c1 > 0)
+<F12>			c1--, dr1 = *(n1 + c1) - 48;
 		else
 			dr1 = 0;
 		if (c2 > 0)
@@ -45,10 +45,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			dr2 = 0;
 		bg--, size_r--;
-	}
-	if (*(r) == '0')
+<F12>	}
+<F12>	if (*(r) == '0')
 		return (r + 1);
 	else
 		return (r);
 }
-
+<F12>
